2dce3502d907f80a748cec881a9501dfeb968e11
imam0321 <imam.hossain0321@gmail.com>
2025-07-29 19:29:29 +0600

    Implement server setup with error handling and routing

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+import cookieParser from "cookie-parser";
+import passport from "passport";
+import "./app/config/passport.config";
+import { router } from "./app/routes";
+import { envVars } from "./app/config/env";
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
+
+const app: Application = express();
+
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(cookieParser());
+app.use(express.json());
+app.use(
+  cors({
+    origin: envVars.FRONTEND_URL,
+    credentials: true,
+  })
+);
+
+// app.use("/api/v1", router);
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Welcome to PaySphere Server",
+  });
+});
+
+app.use(globalErrorHandler);
+app.use(notFound);
+
+export default app;
---
+++ src/app/config/env.ts
@@ -0,0 +1,40 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+interface EnvConfig {
+  PORT: string;
+  DB_URL: string;
+  NODE_ENV: "development" | "production";
+  ADMIN_EMAIL: string;
+  ADMIN_PASSWORD: string;
+  FRONTEND_URL: string;
+}
+
+const loadEnvVariables = (): EnvConfig => {
+  const requiredEnvVariable: string[] = [
+    "PORT",
+    "DB_URL",
+    "NODE_ENV",
+    "ADMIN_EMAIL",
+    "ADMIN_PASSWORD",
+    "FRONTEND_URL",
+  ];
+
+  requiredEnvVariable.forEach((key) => {
+    if (!process.env[key]) {
+      throw new Error(`Missing require environment variable ${key}`);
+    }
+  });
+
+  return {
+    PORT: process.env.PORT as string,
+    DB_URL: process.env.DB_URL as string,
+    NODE_ENV: process.env.NODE_ENV as "development" | "production",
+    ADMIN_EMAIL: process.env.ADMIN_EMAIL as string,
+    ADMIN_PASSWORD: process.env.ADMIN_PASSWORD as string,
+    FRONTEND_URL: process.env.FRONTEND_URL as string,
+  };
+};
+
+export const envVars = loadEnvVariables();
